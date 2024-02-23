32f3932470ae7689c875ad251a95714fbbe252f9
DibyalochanDash <88679295+DibyalochanDash@users.noreply.github.com>
2024-02-24 00:57:34 +0530

    Update Array_Unique_element.java

--- Day_1/Array_Unique_element.java
+++ Day_1/Array_Unique_element.java
@@ -4,7 +4,23 @@ public class Array_Unique_element {
 
 	public static void main(String[] args) {
 		
+		int []arr ={1,2,3,1,2,4};
+
+		FindUniqueElement(arr);
 
 	}
-
+ private static void FindUniqueElement(int arr[]){
+	 for (int i = 0; i < arr.length; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] == arr[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                System.out.println("First unique element: " + arr[i]);
+		     }
+        System.out.println("No unique element found in the array.");
+ }
 }
